bf5439cdc24f3bc03cdfad7da45d55503294d9e2
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-05-20 17:09:44 -0500

    Card fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,32 +5,35 @@ export default function Card({ title, characteristics, price  }) {
     return (
         <>
             <div className="container-card">
-            <div className="sub-container">
-                <div className="content-container">
-                    <div className="header-container">
-                        <h3 className="title">
-                            { title }
-                        </h3>
-                    </div>
-                    <hr />
-                    <div className="footer-container">
-                        <p className="precio">
-                            <span className='signo'>S/.</span> {price}
-                        </p>
-                        <ul className="list-container">
-                            {characteristics.map((item) => (
-                                <li key={item.id}> <FontAwesomeIcon icon={faCheck} /> { item.description } </li>
-                            ))}
-                        </ul>
-                        <div>
-                            <a href="#" className='button'>
-                                Contratar
-                            </a>
+                <div className="sub-container">
+                    <div className="content-container">
+                        <div className="header-container">
+                            <h3 className="title">
+                                { title }
+                            </h3>
+                        </div>
+                        <hr />
+                        <div className="footer-container">
+                            <p className="precio">
+                                <span className='signo'>S/.</span> {price}
+                            </p>
+                            <ul className="list-container">
+                                {characteristics.map((item) => (
+                                    <li key={item.id}>
+                                        <FontAwesomeIcon icon={faCheck} />
+                                        <div className="item-description">{ item.description }</div> 
+                                    </li>
+                                ))}
+                            </ul>
+                            <div>
+                                <a href="#" className='button'>
+                                    Contratar
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
